32b19fa25d58f7f69cb57fba20fc45e6e6b6e85e
Jordan <github@jschr.io>
2022-09-09 15:39:54 -0400

    Updated branding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,37 @@
-const colors = {
-  gray: {
-    850: '#18202F',
-    950: '#000a14',
-  },
-  navy: '#003670',
-  skyBlue: '#009bd2',
-  limeGreen: '#28d972',
-};
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+const colors = {};
 
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}', './content/**/*.{md,mdx}'],
   darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Gellix', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
-        ...colors,
+        gray: {
+          850: '#18202F',
+          950: '#000a14',
+        },
+        sky: {
+          // Generated from https://www.tailwindshades.com/#color=220.09478672985782%2C99.06103286384977%2C41.76470588235294&step-up=10&step-down=7&hue-shift=0&name=science-blue&base-stop=6&overrides=e30%3D
+          DEFAULT: '#0147D4',
+          50: '#EFF4FF',
+          100: '#D5E3FF',
+          200: '#A2C1FF',
+          300: '#709FFE',
+          400: '#3D7DFE',
+          500: '#0A5BFE',
+          600: '#0147D4',
+          700: '#013BB0',
+          800: '#012F8D',
+          900: '#002369',
+        },
         'logo-bg': {
-          DEFAULT: colors.skyBlue,
-          dark: '#29c6ff',
+          DEFAULT: '#000',
+          dark: '#fff',
         },
       },
       screens: {
